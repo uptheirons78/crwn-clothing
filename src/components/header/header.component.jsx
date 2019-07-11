ae6fabfbc413aabcff27f11006334f8d9e6e7e7a
Mauro Bono <bono.mauro@gmail.com>
2019-07-11 14:50:40 +0200

    integrated firebase in project, added sign in and out buttons and functionalities, added sign in with google button, working with OAuth

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
 
 
-function Header() {
+function Header({ currentUser }) {
+  console.log(currentUser);
   return (
     <div className='header'>
       <Link className='logo-container' to='/'>
@@ -17,6 +19,20 @@ function Header() {
         <Link className="option" to='/shop'>
           CONTACT
         </Link>
+        {
+          currentUser
+          ?
+          ( <div className="option" onClick={ () => auth.signOut() }>
+              SIGN OUT
+            </div>
+          )
+          :
+          (
+            <Link className="option" to="/signin">
+              SIGN IN
+            </Link>
+          )
+        }
       </div>
     </div>
   )
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component'
+
+import { signInWithGoogle } from '../../firebase/firebase.utils';
 import './sign-in.styles.scss';
 
 
@@ -51,8 +53,12 @@ export class SignIn extends Component {
             label="password"
             required
           />
-
-          <CustomButton type="submit"> Sign in </CustomButton>
+          <div className="buttons">
+            <CustomButton type="submit"> Sign in </CustomButton>
+            <CustomButton onClick={ signInWithGoogle } isGoogleSignIn>
+              Sign in with Google
+            </CustomButton>
+          </div>
         </form>
       </div>
     )
